4ed984ea6cf4fc4566098817a3f21ebf47c5670a
Adrian Damian Katulski <katulskiadrian@gmail.com>
2020-05-31 21:05:14 +0200

    Added parameters as input

--- load-and-count.py
+++ load-and-count.py
@@ -1,6 +1,12 @@
 import csv
-file = open("private/data.txt")
-#l = ["a", "b", "c", "d", "f", "g", "h", "i", "j"]
+
+# file = open("private/data.txt")
+
+openPath = input("ścieżka do pliku TXT ")
+savePath = input("ścieżka do zapisania plikus CSV ")
+phiw = input("wprowadź Φw ")
+deltaw = input("prowadź Δw ")
+
 data = []
 cutOut = []
 def to_matrix(l, n):
@@ -20,8 +26,10 @@ def fi(phiw, deltax, deltaw):    # lm
 def n(fi, pu):          # efficiency
     return fi/pu
 
-for line in file:
-    data.append(line.rstrip())
+with open(openPath) as f:
+    for line in f:
+        data.append(line.rstrip())
+
 data = to_matrix(data, 6)
 
 for i in range(1, len(data)):
@@ -32,7 +40,7 @@ for i in range(1, len(data)):
     data[i][4] = float(data[i][4].replace(',', '.'))    # lambda
     data[i][5] = float(data[i][5].replace(',', '.'))    # delta x
     data[i].append(float(cosfiu(data[i][3], data[i][1], data[i][2])))  # cosFiu
-    data[i].append(float(fi(641, data[i][5], 94.8)))       # fi
+    data[i].append(float(fi(float(phiw), data[i][5], float(deltaw))))       # fi
     data[i].append(float(n(data[i][7], data[i][3])))       # efficiency
 
 for i in range(0, int(len(data)/10)):
@@ -40,12 +48,11 @@ for i in range(0, int(len(data)/10)):
 
 cutOut[0].extend(['cosfiu [-]', 'fi [lm]', 'efficiency [lm/W]'])
 
-# todo generate CSV and make var from start parameters
 
-with open("private/out.csv", 'w', newline='') as f:
+with open(savePath, 'w', newline='') as f:
     thewriter = csv.writer(f)
     for line in cutOut:
         thewriter.writerow(line)
-
+# todo naprawic błąd zapisu
 print(cutOut)
 # with open("/private/data-low.txt") as file: 
