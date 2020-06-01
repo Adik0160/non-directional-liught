fb3602d973175fdfbb8fe22c71c6022828d7c386
Adrian Damian Katulski <katulskiadrian@gmail.com>
2020-06-01 17:13:18 +0200

    Added fuctionj for counting time from zero

--- load-and-count.py
+++ load-and-count.py
@@ -1,17 +1,14 @@
 import csv
 
-# file = open("private/data.txt")
-
 openPath = input("ścieżka do pliku TXT ")
 savePath = input("ścieżka do zapisania plikus CSV ")
-phiw = input("wprowadź Φw ")
-deltaw = input("prowadź Δw ")
+phiw = input("wprowadź Φw (strimień świetlny wzorca) ")
+deltaw = input("prowadź Δw (Wskazanie miernika prądu fotoelektrycznego) ")
 
 data = []
 cutOut = []
 def to_matrix(l, n):
     return [l[i:i + n] for i in range(0, len(l), n)]
-# data = file.readlines()
 
 def time_to_sec(str):
     h, m, s = str.split(':')
@@ -33,7 +30,7 @@ with open(openPath) as f:
 data = to_matrix(data, 6)
 
 for i in range(1, len(data)):
-    data[i][0] = time_to_sec(data[i][0])                # time
+    data[i][0] = int(time_to_sec(data[i][0]))           # time
     data[i][1] = float(data[i][1].replace(',', '.'))    # U
     data[i][2] = float(data[i][2].replace(',', '.'))    # I
     data[i][3] = float(data[i][3].replace(',', '.'))    # P
@@ -43,8 +40,14 @@ for i in range(1, len(data)):
     data[i].append(float(fi(float(phiw), data[i][5], float(deltaw))))       # fi
     data[i].append(float(n(data[i][7], data[i][3])))       # efficiency
 
+firstTime = int(0)
+
 for i in range(0, int(len(data)/10)):
     cutOut.append(data[i*10])
+    if i == 1:
+        firstTime = cutOut[i][0]
+    if i >= 1:
+        cutOut[i][0] -= firstTime
 
 cutOut[0].extend(['cosfiu [-]', 'fi [lm]', 'efficiency [lm/W]'])
 
@@ -53,6 +56,4 @@ with open(savePath, 'w', newline='') as f:
     thewriter = csv.writer(f)
     for line in cutOut:
         thewriter.writerow(line)
-# todo naprawic błąd zapisu
 print(cutOut)
-# with open("/private/data-low.txt") as file: 
