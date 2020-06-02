b24259d97facc1e21b3b7287a28959030361aab2
Adrian Damian Katulski <katulskiadrian@gmail.com>
2020-06-02 17:37:20 +0200

    Added a switch to change a number of columns in matrix (witch lambda parameter or no)

--- load-and-count.py
+++ load-and-count.py
@@ -4,6 +4,7 @@ openPath = input("ścieżka do pliku TXT ")
 savePath = input("ścieżka do zapisania plikus CSV ")
 phiw = input("wprowadź Φw (strimień świetlny wzorca) ")
 deltaw = input("prowadź Δw (Wskazanie miernika prądu fotoelektrycznego) ")
+lambada = int(input("lambda? 0/1"))
 
 data = []
 cutOut = []
@@ -27,18 +28,29 @@ with open(openPath) as f:
     for line in f:
         data.append(line.rstrip())
 
-data = to_matrix(data, 6)
+if lambada:
+    data = to_matrix(data, 6)
+else:
+    data = to_matrix(data, 5)
 
 for i in range(1, len(data)):
     data[i][0] = int(time_to_sec(data[i][0]))           # time
     data[i][1] = float(data[i][1].replace(',', '.'))    # U
     data[i][2] = float(data[i][2].replace(',', '.'))    # I
     data[i][3] = float(data[i][3].replace(',', '.'))    # P
-    data[i][4] = float(data[i][4].replace(',', '.'))    # lambda
-    data[i][5] = float(data[i][5].replace(',', '.'))    # delta x
-    data[i].append(float(cosfiu(data[i][3], data[i][1], data[i][2])))  # cosFiu
-    data[i].append(float(fi(float(phiw), data[i][5], float(deltaw))))       # fi
-    data[i].append(float(n(data[i][7], data[i][3])))       # efficiency
+    if lambada:
+        data[i][4] = float(data[i][4].replace(',', '.'))    # lambda
+        data[i][5] = float(data[i][5].replace(',', '.'))    # delta x
+
+        data[i].append(float(cosfiu(data[i][3], data[i][1], data[i][2])))  # cosFiu
+        data[i].append(float(fi(float(phiw), data[i][5], float(deltaw))))  # fi
+        data[i].append(float(n(data[i][7], data[i][3])))  # efficiency
+    else:
+        data[i][4] = float(data[i][4].replace(',', '.'))  # delta x
+        data[i].append(float(cosfiu(data[i][3], data[i][1], data[i][2])))  # cosFiu
+        data[i].append(float(fi(float(phiw), data[i][4], float(deltaw))))  # fi
+        data[i].append(float(n(data[i][6], data[i][3])))  # efficiency
+
 
 firstTime = int(0)
 
